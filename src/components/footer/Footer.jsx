f7cdf364a6f860b058c19f3684a2427537ede5d1
tanishkaadhav30 <tanishkaadhav30@gmail.com>
2026-08-23 12:11:45 +0530

    files added

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.css";
+import { FiHome,FiMapPin,FiPhoneCall,FiMessageCircle } from "react-icons/fi";
+
+function Footer() {
+  return (
+    <footer className="footer">
+
+      <div className="footer-container">
+
+        {/* Brand */}
+        <div className="footer-brand">
+        
+          <h2>
+            <FiHome/> <span> NestFinder </span>
+          </h2>
+  
+          <p>
+            Find your dream home with trusted properties,
+            better locations and a simple property search.
+          </p>
+        </div>
+
+
+        {/* Quick Links */}
+        <div className="footer-column">
+
+          <h3>Quick Links</h3>
+
+          <Link to="/">Home</Link>
+          <Link to="/properties">Properties</Link>
+          <Link to="/wishlist">Wishlist</Link>
+          <Link to="/about">About</Link>
+
+        </div>
+
+
+        {/* Customer Service */}
+        <div className="footer-column">
+
+          <h3>Popular Cities</h3>
+
+          <Link to="/properties">Mumbai</Link>
+          <Link to="/properties">Pune</Link>
+          <Link to="/properties">Nashik</Link>
+          <Link to="/properties">Nagpur</Link>
+
+        </div>
+        {/* Contact */}
+        <div className="footer-column contact-column">
+
+          <h3>Contact Us</h3>
+
+          <a href="mailto:tanishkaadhav30@gmail.com">
+            <FiMessageCircle/>
+              Email Us</a>
+
+          <a href="tel:+919209039114"><FiPhoneCall/> Call Us</a>
+
+          <a href="https://maps.app.goo.gl/Ece1sgJRdGnsWjRR9" target="_blank" rel="noopener noreferrer">
+            <FiMapPin/> Location
+          </a>
+
+        </div>
+
+      </div>
+
+
+      {/* Bottom */}
+      <div className="footer-bottom">
+
+        <p>
+          © 2026 NestFinder. All Rights Reserved.
+        </p>
+
+      </div>
+
+    </footer>
+  );
+}
+
+export default Footer;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,168 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Navbar.css";
+import { FiHome, FiHeart, FiInfo, FiLogIn } from "react-icons/fi";
+import { FiMenu, FiSun, FiMoon } from "react-icons/fi";
+
 function Navbar() {
-    return (
-        <nav>
-        <h2>PropFinder</h2>
-        </nav>
-    );
+  const navigate = useNavigate();
+  // Hamburger
+  const [menuOpen, setMenuOpen] = useState(false);
+  // Day / Night
+  const [darkMode, setDarkMode] = useState(false);
+
+
+  // ===// DAY / NIGHT // =====//
+  const toggleTheme = () => {
+
+    setDarkMode((prev) => {
+      const newMode = !prev;
+      document.body.classList.toggle(
+        "dark-mode",
+        newMode
+      );
+
+      return newMode;
+    });
+  };
+
+  // =========// CLOSE MENU// =====//
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
+  return (
+
+    <nav className="navbar">
+
+      {/* ===== LOGO ====== */}
+
+      <div
+        className="navbar-logo"
+        onClick={() => navigate("/")}
+      >
+        <FiHome />
+        NestFinder
+      </div>
+
+      {/* ===== DESKTOP MENU ===== */}
+
+      <div className="navbar-links">
+
+        <button
+          onClick={() => navigate("/")} >
+          <FiHome />
+          Home
+        </button>
+
+        <button
+          onClick={() => navigate("/properties")} >
+          <FiHome />
+          Properties
+        </button>
+
+
+        <button
+          onClick={() => navigate("/about")} >
+          <FiInfo />
+          About
+        </button>
+
+
+        <button
+          onClick={() => navigate("/Wishlist")} >
+          <FiHeart />
+          Wishlist
+        </button>
+
+        <button
+          onClick={() => navigate("/Login")} >
+          <FiLogIn />
+          Login
+        </button>
+
+
+        {/* Desktop Theme */}
+
+        <button
+          className="theme-btn"
+          onClick={toggleTheme} >
+          {darkMode ? <FiSun /> : <FiMoon />}
+        </button>
+      </div>
+
+      {/* ================= HAMBURGER ================= */}
+
+      <button
+        className="hamburger-btn"
+        onClick={() => setMenuOpen(!menuOpen)
+        }
+        aria-label="Toggle menu"
+      >
+        {menuOpen ? <FiHome/> : <FiMenu/>}
+      </button>
+
+      {/* ================= MOBILE MENU ================= */}
+
+      <div
+        className={
+          `mobile-menu ${menuOpen
+            ? "mobile-menu-open"
+            : ""
+          }`
+        }
+      >
+
+        <button
+          onClick={() => {
+            navigate("/");
+            closeMenu();
+          }}
+        >
+          Home
+        </button>
+
+        <button
+          onClick={() => {
+            navigate("/properties");
+            closeMenu();
+          }}
+        >
+          Properties
+        </button>
+
+        <button
+          onClick={() => {
+            navigate("/about");
+            closeMenu();
+          }}
+        >
+          About
+        </button>
+
+        <button
+          onClick={() => {
+            navigate("/login");
+            closeMenu();
+          }}
+        >
+          Login
+        </button>
+
+        {/* ================= MOBILE THEME ================= */}
+
+        <button
+          className="mobile-theme-btn"
+          onClick={toggleTheme}
+        >
+          {darkMode
+            ? "☀️ Day Mode"
+            : "🌙 Night Mode"}
+        </button>
+      </div>
+    </nav>
+  );
 }
+
 export default Navbar;
